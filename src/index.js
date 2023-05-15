ad2ecb90ea6eb888692b1acc9871edd5a893031d
1TSpahc <3lprxd@gmail.com>
2023-05-15 18:14:42 -0500

    CORS

--- src/index.js
+++ src/index.js
@@ -2,9 +2,18 @@ require('dotenv').config()
 
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors({
+  origin: 'https://buho-caso-7-api-production.up.railway.app', // Cambia esto con el origen permitido para tu cliente
+  methods: ['GET', 'POST'], // Métodos de solicitud permitidos
+  allowedHeaders: ['Content-Type'] // Encabezados permitidos
+}
+))
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
